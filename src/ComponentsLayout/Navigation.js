3780b6ca1c688aa06833586e945a8e7eec35a95e
Snikerso <42245963+Snikerso@users.noreply.github.com>
2020-01-25 18:23:53 +0100

    nawigacja

--- src/ComponentsLayout/Navigation.js
+++ src/ComponentsLayout/Navigation.js
@@ -1,8 +1,14 @@
-import './App.css';
+import React from 'react'
+import {Link}from 'react-router-dom'
 
 function Navigation() {
   return (
-    <div></div>
+    <>
+        <Link>Budynki</Link>
+        <Link>Badania</Link>
+        <Link>Dom Genetyki</Link>
+
+    </>
   );
 }
 
